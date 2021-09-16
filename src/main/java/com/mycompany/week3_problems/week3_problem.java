cf4b71a5fa3503c0f3f6e302de417b81006200c7
lrg1031@live.com <>
2021-09-16 14:38:12 -0400

    added getPrimeSum method to current, gets sum of all primes in given array

--- src/main/java/com/mycompany/week3_problems/week3_problem.java
+++ src/main/java/com/mycompany/week3_problems/week3_problem.java
@@ -2,7 +2,7 @@ package com.mycompany.week3_problems;
 
 public class week3_problem {
     
-    boolean isPrime(int n) {
+    public boolean isPrime(int n) {
         for(int i = 2; i <= n-1; i++) {
             if(n % i == 0) {
                 return false;
@@ -11,10 +11,22 @@ public class week3_problem {
         return true;
     }
     
+    public int getPrimeSum(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i<nums.length; i++) {
+            if(isPrime(nums[i])) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+    
     public static void main(String[] args) {
         week3_problem nc = new week3_problem();
-        boolean answer = nc.isPrime(16);
-        System.out.println(answer);
+        int x = 5;
+        int[] arr = {3,4,8,7,9,10,12,5};
+        int sum = nc.getPrimeSum(arr);
+        System.out.println("Sum of primes in array is " + sum);
         
     }
     
